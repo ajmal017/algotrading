fe306987eb37b69eb46ba85eeb664eadd8a95bbd
Trent <trentcrawley@gmail.com>
2021-06-27 18:48:21 +1000

    start working on trend change

--- scratch.py
+++ scratch.py
@@ -1,34 +1,31 @@
+import connectdb
+import numpy as np
+import pandas as pd
+import stockplot as sp
+import imp
+pd.options.mode.chained_assignment = None
+import filters
+import features as ft
+import datetime
 
+imp.reload(ft)
+imp.reload(sp)
+dbconn = connectdb.connectdb()
+dbconn.conn = dbconn.pgconnect()
 
+query = """
+SELECT * FROM asxminutedata where datetime > '31/10/2019' AND datetime < '29/02/2020' AND
+ticker in ('APT')
+"""
 
-df.reset_index(inplace=True)
-rolling = pf.ols.PandasRollingOLS(y = df.close,x = df.index,window = 15)
-beta = rolling.beta
-sterr = rolling.std_err
-alpha = rolling.alpha
-rollingreg = pd.concat([beta,sterr,alpha],axis =1).rename(columns = {'feature1':'beta15','std_err':'stderr15'})
-df = df.merge(rollingreg,how = 'left',left_index=True,right_index=True)
+x = pd.read_csv('apt.csv')
+colnames = ['open', 'high', 'low', 'close', 'volume', 'value', 'count', 'datetime', 'ticker']
 
-stdevfilter = np.where(df.stderr15<1,1,0)
-betafilterdown = np.where((df.beta15<=-0.4) ,1,0)
+df = pd.DataFrame(x, columns=colnames).set_index('datetime')
+df = df.sort_values(by=['ticker', 'datetime'], ascending=[True, True])
+df.index = pd.to_datetime(df.index)
+df['date'] = df.index.date
+df['time'] = df.index.time
 
-
-df['signal'] = np.where(stdevfilter & betafilterdown,1,0)
-
-df2 = df[df['signal']==1]
-df2 = df2[['date','ticker']]
-
-data = pd.merge(df,df2.drop_duplicates(),how ='inner',left_on=['date','ticker'],right_on=['date','ticker'])
-data['closesignal'] =0
-data.ticker.unique()
-test = sp.stockplots(data[data['ticker']=='APT'])
-test.multiplot(4,7)
-test.singleplot()
-test.showplot()
-
-
-
-
-
-
-
+plot = sp.stockplots(df)
+plot.multiplot(3,4)
